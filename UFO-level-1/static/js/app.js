9a092502db8f9d996b39db96bd57302e33bc7333
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-09 10:24:34 -0400

    UFO-level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,6 +3,11 @@ var $dateInput = document.querySelector("#datetime");
 var $searchBtn = document.querySelector("#search");
 var $resetBtn = document.querySelector("#reset");
 
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+$resetBtn.addEventListener("click", handleResetButtonClick);
+
 // from data.js
 var tableData = data;
+
 
