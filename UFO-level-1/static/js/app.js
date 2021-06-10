30a2286522f21183181a05bc1ed87d2be3a2235a
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-10 01:18:28 -0400

    UFO-level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,12 +3,29 @@ var $dateInput = document.querySelector("#datetime");
 var $searchBtn = document.querySelector("#search");
 var $resetBtn = document.querySelector("#reset");
 
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-
-$resetBtn.addEventListener("click", handleResetButtonClick);
-
 // from data.js
 var tableData = data;
+
+var button = d3.select("#button")
+
+var form = d3.select("#form")
+
+button.on("click", runEnter)
+form.on("submit", runEnter)
+
+function runEnter(event) {
+  event.preventDefault()
+
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property('value');
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(city => city.datetime === inputValue);
+  
+  console.log(filteredData);
+}
 
 function renderTable() {
     $tbody.innerHTML = "";
