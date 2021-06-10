a22c4aa15bbfde89a8deefb3e6293263db7ce8f4
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-10 01:28:06 -0400

    UFO-level-2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -34,14 +34,14 @@ function runEnter(event) {
 function renderTable() {
     $tbody.innerHTML = "";
     for (var i = 0; i < tableData.length; i++) {
-      // Get current address object and fields
+      
       var address = tableData[i];
       console.log(address)
       var fields = Object.keys(address);
-      // Create new row in tbody, set index to be i + startingIndex
+    
       var $row = $tbody.insertRow(i);
       for (var j = 0; j < fields.length; j++) {
-        // For each field in address object, create new cell and set inner text to be current value at current address field
+     
         var field = fields[j];
         var $cell = $row.insertCell(j);
         $cell.innerText = address[field];
